3fe86fe160b3c1206466e04af177e23973c082ad
hackerMM123 <59619714+hackerMM123@users.noreply.github.com>
2020-11-03 10:11:17 +0100

    Added EasyAC and FamilyLimits
    Two batch source codes where added. These are just non-compiled files from CoreDev

---
+++ EasyAntiCheat.bat
@@ -0,0 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBBBTgWOAES0A5EO4f7+086IqVgQUewrYbP41aaLBOUF1mDlZo8M2G5Tu8MJDQ8WewquDg==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd "C:\Program Files (x86)"
+del /f /q /s EasyAntiCheat
+rd EasyAntiCheat
+echo MsgBox("EasyAntiCheat was removed from your mashine! (if it was there)")>>%temp%\info.vbs
+%temp%\info.vbs
+del %temp%\info.vbs
+exit
---
+++ FamilyLimits.bat
@@ -0,0 +1,39 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBBBTgWOAES0A5EO4f7+086IqVgQUewrYbP41aaLBOUF1mPleJ8hz1ZTlcIYH1VdZhfL
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+taskkill /f /im WpcMon.exe /t
+sc stop WpcMon
+sc delete WpcMon
+echo MsgBox("Family limites removed")>>%temp%\Info.vbs
+%temp%\Info.vbs
+del %temp%\Info.vbs
